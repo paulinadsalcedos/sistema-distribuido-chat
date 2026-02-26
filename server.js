b7f7dc6e1b6bc73ebd8402ff35a9f2465f8a1f90
notpaulinita <paulina.d.salcedo.s@gmail.com>
2026-02-26 13:42:30 -0600

    version definitiva

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ io.on("connection", (socket) => {
   });
 });
 
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
